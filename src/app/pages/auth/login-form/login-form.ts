e0f99ab88166ce9b5ab35f031cd1767a8862baaa
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-10-04 10:31:39 -0300

    feat: add login auth

--- src/app/pages/auth/login-form/login-form.ts
+++ src/app/pages/auth/login-form/login-form.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../shared/services/auth/auth-service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login-form.html',
-  styleUrl: './login-form.css'
+  styleUrl: './login-form.css',
 })
 export class LoginForm {
+  formGroup: FormGroup;
 
+  constructor(private authService: AuthService, private router: Router) {
+    this.formGroup = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    if (this.formGroup.valid) {
+      const data = this.formGroup.value;
+      this.authService.login(data);
+    }
+  }
 }
--- src/app/shared/services/auth/auth-service.ts
+++ src/app/shared/services/auth/auth-service.ts
@@ -2,20 +2,42 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Auth } from '../../models/auth';
 import { environment } from '../../../../environments/environment.development';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  api = `${environment.api}/token/`;
 
-  api = `${environment.api}/auth/`
-
-  constructor(private clienteHttp: HttpClient) {}
+  constructor(private clienteHttp: HttpClient, private router: Router) {}
 
   login(data: Auth) {
-    return this.clienteHttp.post(this.api, data)
+    return this.clienteHttp.post(this.api, data).subscribe({
+      next: (response) => {
+        localStorage.setItem('access_token', (response as any).access);
+        localStorage.setItem('refresh_token', (response as any).refresh);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Login error', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Usuário e/ou senha inválidos!',
+        });
+      },
+    });
   }
 
-  
-  
+  refreshToken(refresh: string) {
+    return this.clienteHttp.post(`${this.api}refresh/`, { refresh });
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    this.router.navigate(['/auth']);
+  }
 }
